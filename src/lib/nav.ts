33a6a67bae2db6a03a0405100a889950a922eef4
Max Beatty <githublove@maxbeatty.com>
2026-04-12 16:59:31 -0700

    Update profile image, refine About page content, and enhance Projects section with new entries

--- src/lib/nav.ts
+++ src/lib/nav.ts
@@ -2,5 +2,4 @@ export const navItems = [
   { href: "/about/", label: "About" },
   { href: "/articles/", label: "Articles" },
   { href: "/projects/", label: "Projects" },
-  { href: "/uses/", label: "Uses" },
 ];
